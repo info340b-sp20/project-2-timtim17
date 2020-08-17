dcb6ce2fe22545564fc0583de2112d056075ae23
Austin Jenchi <ajenchi9@gmail.com>
2020-08-17 06:39:45 -0700

    Add _blank rel attributes per compile linter

--- src/MetaLinks.js
+++ src/MetaLinks.js
@@ -4,7 +4,8 @@ import { faGithub } from '@fortawesome/free-brands-svg-icons';
 
 export function SourceCodeLink({ message='Source Code', showIcon=true, id, className }) {
   return (
-    <a href="https://github.com/info340b-sp20/project-2-timtim17" target="_blank" id={id} className={className}>
+    <a href="https://github.com/info340b-sp20/project-2-timtim17" target="_blank"
+      rel="noopener noreferrer" id={id} className={className}>
       {showIcon &&<FontAwesomeIcon icon={faGithub} />} { message }
     </a>
   );
